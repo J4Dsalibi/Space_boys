3d459f7cfbfddb304987e6f8c450366c73ff3cff
J4Dsalibi <145556642+J4Dsalibi@users.noreply.github.com>
2023-09-21 17:53:32 +0200

    Update space.py

--- documents/space.py
+++ documents/space.py
@@ -3,4 +3,11 @@ import pygame  # necessaire pour charger les images et les sons
 
 class Joueur() : # classe pour créer le vaisseau du joueur
     def __init__(self) :
-        pass
+        self.sens = "O"
+        self.position = 400
+        self.image = pygame.image.load('vaisseau.png')
+    def deplacer(self):
+        if self.sens=='gauche':
+            self.position-=0.5
+        elif self.sens=='droite':
+            self.position+=0.5
